3c51f9896226849068e293b8d9a240ac75da7809
yehuda-sanay <yehudasanay1@gmail.com>
2022-07-04 11:20:54 +0300

    04.07.22

--- main.js
+++ main.js
@@ -11,10 +11,11 @@ async function getApi(){
 async function printToConsole(){
     let devices = await getApi()
     devices.forEach(device => {
-        console.log(device)
+        // console.log(device)
         
     });
 }
+printToConsole()
 async function printToscreen(){
     let devices = await getApi()
     devices.forEach(device => {
@@ -29,17 +30,27 @@ printToscreen()
 function cardTemplate(device){
     
     document.getElementById("phones_cards").innerHTML+=`
-    <div class="card">
-    <img src="${device.picture}" style="width:100%;">
+    <div class="card" id="${device.id}">
+    <img src="${device.picture}" style= "width:100%;">
     <h5>brand : ${device.brand}</h5>
     <p>isAvailable : ${device.isAvailable}</p>
     <p>createdAt : ${device.createdAt}</p>
     <p>color : ${device.color}</p>
     <p>price : ${device.price}</p>
     <p>ram : ${device.ram}</p>
+    <button onclick="deletePhone('${device.id}')">Remove</button>
   
     </div>
     `
+}
+
+async function deletePhone(id){
+    try{
+        let result = await fetch(`${API_BASIC}/${id}`,{method:"DELETE"})
+        .then(res=>res.json())
+        if(result) document.getElementById(id).remove()
+    } catch(error){}
+    finally{}
 }
 
     
